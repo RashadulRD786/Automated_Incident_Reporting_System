b401e23138db728bbf2ddbc7400d9547bb580da9
Rashadul Nafis Riyad <nafisrashadul@gmail.com>
2026-05-21 20:37:36 +0800

    Deployed at Vercel

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: [
+    process.env.FRONTEND_URL,
+    'http://localhost:3000',
+    
+  ],
   credentials: true,
 }));
 app.use(express.json());
